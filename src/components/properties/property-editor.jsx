29409182a8b64ccb9199c5fb30dcbeb6b269274c
Koustov Maitra <koustov@live.com>
2021-06-05 21:10:36 +0530

    fixes

--- src/components/properties/property-editor.jsx
+++ src/components/properties/property-editor.jsx
@@ -1,162 +1,198 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
+import * as React from 'react'
+
 import {
   FPHeaderBar,
-  FPMediumHeader, FPMediumHeaderBar, FPTabs, FPTabWrapper, SmallHeader, FPPaperVerticalPadding, FPPaper
-} from '../styled';
-import { getFinalField } from './fields';
-import { FaSave, FaTimes } from 'react-icons/fa';
-import Fab from '@material-ui/core/Fab';
+  FPMediumHeader,
+  FPMediumHeaderBar,
+  FPPaper,
+  FPPaperVerticalPadding,
+  FPTabWrapper,
+  FPTabs,
+  SmallHeader
+} from '../styled'
+import { FaSave, FaTimes } from 'react-icons/fa'
+import { useEffect, useState } from 'react'
 
-import Tab from '@material-ui/core/Tab';
-import Box from '@material-ui/core/Box';
-
+import Box from '@material-ui/core/Box'
+import Fab from '@material-ui/core/Fab'
 import { FormViewer } from '../../FormViewer'
+import Tab from '@material-ui/core/Tab'
+import { getFinalField } from './fields'
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props
 
   return (
     <div
-      role="tabpanel"
+      role='tabpanel'
       hidden={value !== index}
       id={`vertical-tabpanel-${index}`}
       aria-labelledby={`vertical-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box p={0}>
-          {children}
-        </Box>
-      )}
+      {value === index && <Box p={0}>{children}</Box>}
     </div>
-  );
+  )
 }
 
 export const PropertyEditor = ({ control, onChange, onClose }) => {
-  const [controlState, setControlState] = useState(control);
-  const [editContainerGroups, setEditContainerGroups] = useState({});
-  const [selectedTabIndex, setSelectedTabIndex] = React.useState(0);
-
+  const [controlState, setControlState] = useState(control)
+  const [editContainerGroups, setEditContainerGroups] = useState({})
+  const [selectedTabIndex, setSelectedTabIndex] = React.useState(0)
 
   useEffect(() => {
     if (control) {
-      const res = {};
+      const res = {}
       control.editableFields.forEach((ed) => {
-        const groupName = ed.group ? ed.group : "Default";
+        const groupName = ed.group ? ed.group : 'Default'
         if (!res[groupName]) {
-          res[groupName] = [ed];
+          res[groupName] = [ed]
         } else {
-          res[groupName].push(ed);
+          res[groupName].push(ed)
         }
       })
-      setEditContainerGroups(res);
+      setEditContainerGroups(res)
     }
   }, [control])
 
-
   const handleTabSelectionChange = (_event, newValue) => {
-    setSelectedTabIndex(newValue);
-  };
+    setSelectedTabIndex(newValue)
+  }
 
   const onValueChanged = (_key, value, field) => {
     if (field.is_custom) {
-      controlState.custom = controlState.custom || {};
-      controlState.custom[field.targetField] = value;
+      controlState.custom = controlState.custom || {}
+      controlState.custom[field.targetField] = value
     } else {
-      controlState[field.targetField] = value;
+      controlState[field.targetField] = value
     }
     setControlState({ ...controlState })
-  };
+  }
 
   const onSave = () => {
     if (onChange) {
-      onChange(controlState);
+      onChange(controlState)
     }
-    onCancel();
+    onCancel()
   }
 
   const onCancel = () => {
     if (onClose) {
-      onClose();
+      onClose()
     }
   }
 
   const getFieldControl = (field) => {
-
-    return getFinalField(field, onValueChanged, controlState[field.targetField], undefined, field.targetField);
+    return getFinalField(
+      field,
+      onValueChanged,
+      controlState[field.targetField],
+      undefined,
+      field.targetField
+    )
   }
 
   const onControlValueChanged = (key, value, field) => {
-    onValueChanged(key, value, field);
+    onValueChanged(key, value, field)
   }
 
   return (
-    <FPPaperVerticalPadding>
+    <FPPaperVerticalPadding style={{ height: '100%' }}>
       <div>
         <FPMediumHeaderBar>
-          <div className="header-title">
+          <div className='header-title'>
             <FPMediumHeader>Type: {control.typeDisplay}</FPMediumHeader>
           </div>
-          <div className="header-tool-bar">
-            <Fab size="small" variant="extended" color="secondary" aria-label="save" onClick={() => onSave()}>
+          <div className='header-tool-bar'>
+            <Fab
+              size='small'
+              variant='extended'
+              color='secondary'
+              aria-label='save'
+              onClick={() => onSave()}
+            >
               <FaSave />
             </Fab>
-            <Fab size="small" color="default" variant="extended" aria-label="save" onClick={() => onCancel()}>
+            <Fab
+              size='small'
+              color='default'
+              variant='extended'
+              aria-label='save'
+              onClick={() => onCancel()}
+            >
               <FaTimes />
             </Fab>
           </div>
         </FPMediumHeaderBar>
       </div>
-
-      <div>
-        <FPPaper>
-          <FPHeaderBar>
-            <SmallHeader>Controlled Preview</SmallHeader>
-          </FPHeaderBar>
-          {getFinalField(controlState)}
-        </FPPaper>
-      </div>
-      <div className="flex-1">
-        <FPPaper>
-          <FPHeaderBar>
-            <SmallHeader>Update Properties</SmallHeader>
-          </FPHeaderBar>
-          <FPTabWrapper>
-
-            <FPTabs
-              orientation="vertical"
-              variant="scrollable"
-              value={selectedTabIndex}
-              onChange={handleTabSelectionChange}
-              aria-label="Control editor"
+      <div style={{ display: 'flex', flex: 1 }}>
+        <div style={{ width: '30%', padding: '8px' }}>
+          <FPPaper
+            style={{ display: 'flex', flexDirection: 'column', height: '100%' }}
+          >
+            <FPHeaderBar>
+              <SmallHeader>Controlled Preview</SmallHeader>
+            </FPHeaderBar>
+            <div
+              style={{
+                display: 'flex',
+                flex: 1
+              }}
             >
-              {
-                Object.keys(editContainerGroups).map((group, groupi) => {
-                  return <Tab label={`${group}`} key={groupi} />
-                })
-              }
-
-            </FPTabs>
-            <div style={{ overflow: 'auto', flex: 1 }}>
-              {
-                Object.keys(editContainerGroups).map((group, groupi) => {
-                  return <TabPanel key={groupi} value={selectedTabIndex} index={groupi} style={{ flex: 1, padding: '8px' }}>
-                    {
-                      <FormViewer templates={editContainerGroups[group]} onControlValueChanged={onControlValueChanged} />
-                      // editContainerGroups[group].map((control_filed, control_filed_index) => {
-                      //   return <div key={`group_control_${control_filed_index}`}>
-                      //     {getFieldControl(control_filed)}
-                      //   </div>
-                      // })
-                    }</TabPanel>
-                })
-              }
+              {getFinalField(controlState)}
             </div>
-          </FPTabWrapper>
-        </FPPaper>
+          </FPPaper>
+        </div>
+        <div style={{ flex: 1, padding: '8px' }}>
+          <FPPaper style={{ height: '100%' }}>
+            <FPHeaderBar>
+              <SmallHeader>Update Properties</SmallHeader>
+            </FPHeaderBar>
+            <FPTabWrapper>
+              <FPTabs
+                variant='scrollable'
+                value={selectedTabIndex}
+                onChange={handleTabSelectionChange}
+                aria-label='Control editor'
+                indicatorColor='primary'
+                textColor='primary'
+              >
+                {Object.keys(editContainerGroups).map((group, groupi) => {
+                  return (
+                    <Tab label={`${group}`} key={groupi}>
+                      XXXXX {groupi}
+                    </Tab>
+                  )
+                })}
+              </FPTabs>
+              {/* <div style={{ overflow: 'auto', flex: 1 }}>
+                {Object.keys(editContainerGroups).map((group, groupi) => {
+                  return (
+                    <TabPanel
+                      key={groupi}
+                      value={selectedTabIndex}
+                      index={groupi}
+                      style={{ flex: 1, padding: '8px' }}
+                    >
+                      {
+                        <FormViewer
+                          templates={editContainerGroups[group]}
+                          onControlValueChanged={onControlValueChanged}
+                        />
+                        // editContainerGroups[group].map((control_filed, control_filed_index) => {
+                        //   return <div key={`group_control_${control_filed_index}`}>
+                        //     {getFieldControl(control_filed)}
+                        //   </div>
+                        // })
+                      }
+                    </TabPanel>
+                  )
+                })}
+              </div> */}
+            </FPTabWrapper>
+          </FPPaper>
+        </div>
       </div>
     </FPPaperVerticalPadding>
-
-  );
+  )
 }
